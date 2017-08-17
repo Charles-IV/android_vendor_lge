0d90fba704cd4a8ead6ba6cbab1b4117f4ae4087
dadziokPL <danielmania93@gmail.com>
2017-08-17 13:36:23 +0200

    Fixed for c50n

--- c50-common/Android.mk
+++ c50-common/Android.mk
@@ -16,7 +16,7 @@
 
 LOCAL_PATH := $(call my-dir)
 
-ifneq ($(filter h345 ms345,$(TARGET_DEVICE)),)
+ifneq ($(filter c50n h345 ms345,$(TARGET_DEVICE)),)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := com.qualcomm.location
